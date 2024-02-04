b962a610cc52043d8ba0956a7a2471d81b413c8d
ktuz9v <kiram.arhitector1@yandex.ru>
2024-02-04 17:58:33 +0300

    Experiment№1
    Фотореалистичный поворот камеры при хождении впарво или влево

--- Assets/Scripts/CameraRotation.cs
+++ Assets/Scripts/CameraRotation.cs
@@ -20,5 +20,10 @@ public class CameraRotation : MonoBehaviour
             newAngleX -= 360;
         newAngleX = Mathf.Clamp(newAngleX, minAngle, maxAngle);
         CameraAxisTransform.localEulerAngles = new Vector3(newAngleX, 0, 0); //вращение камеры по у
+
+        if (Input.GetKey(KeyCode.A))
+            CameraAxisTransform.localEulerAngles = new Vector3(CameraAxisTransform.localEulerAngles.x, CameraAxisTransform.localEulerAngles.y, 3); //фотореалистичный поворот камеры при повороте направо
+        if (Input.GetKey(KeyCode.D))
+            CameraAxisTransform.localEulerAngles = new Vector3(CameraAxisTransform.localEulerAngles.x, CameraAxisTransform.localEulerAngles.y, -3); //фотореалистичный поворот камеры при повороте налево
     }
 }
