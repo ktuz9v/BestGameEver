865de5075b5b3c30c8f784335e44f46325855133
ktuz9v <kiram.arhitector1@yandex.ru>
2024-02-04 11:01:33 +0300

    CameraRotationWithComments
    Добавлены комментарии

--- Assets/Scripts/CameraRotation.cs
+++ Assets/Scripts/CameraRotation.cs
@@ -4,21 +4,21 @@ using UnityEngine;
 
 public class CameraRotation : MonoBehaviour
 {
-    public float RotationSpeed = 5;
-    public Transform CameraAxisTransform;
+    public float RotationSpeed = 5; //чуствительность мышки
+    public Transform CameraAxisTransform; //обозначение объекта для того, чтобы вращать камеру по у
 
-    public float minAngle;
-    public float maxAngle;
+    public float minAngle; //мин угол наклона по у
+    public float maxAngle; //макс угол наклона по у
 
     void Update()
     {
         var newAngleY = transform.localEulerAngles.y + Time.deltaTime * RotationSpeed * Input.GetAxis("Mouse X");
-        transform.localEulerAngles = new Vector3(0, newAngleY, 0);
+        transform.localEulerAngles = new Vector3(0, newAngleY, 0); //вращение камеры по х
 
         var newAngleX = CameraAxisTransform.localEulerAngles.x + Time.deltaTime * -RotationSpeed * Input.GetAxis("Mouse Y");
         if (newAngleX > 180)
             newAngleX -= 360;
         newAngleX = Mathf.Clamp(newAngleX, minAngle, maxAngle);
-        CameraAxisTransform.localEulerAngles = new Vector3(newAngleX, 0, 0);
+        CameraAxisTransform.localEulerAngles = new Vector3(newAngleX, 0, 0); //вращение камеры по у
     }
 }
