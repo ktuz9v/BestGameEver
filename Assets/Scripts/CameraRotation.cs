13fd5fc15e70b743f5b2ceaf878d97ed593d6c8e
ktuz9v <kiram.arhitector1@yandex.ru>
2024-02-04 10:53:34 +0300

    CameraRotation
    Осуществлено вращение камеры, обозначены ее пределы.

---
+++ Assets/Scripts/CameraRotation.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotation : MonoBehaviour
+{
+    public float RotationSpeed = 5;
+    public Transform CameraAxisTransform;
+
+    public float minAngle;
+    public float maxAngle;
+
+    void Update()
+    {
+        var newAngleY = transform.localEulerAngles.y + Time.deltaTime * RotationSpeed * Input.GetAxis("Mouse X");
+        transform.localEulerAngles = new Vector3(0, newAngleY, 0);
+
+        var newAngleX = CameraAxisTransform.localEulerAngles.x + Time.deltaTime * -RotationSpeed * Input.GetAxis("Mouse Y");
+        if (newAngleX > 180)
+            newAngleX -= 360;
+        newAngleX = Mathf.Clamp(newAngleX, minAngle, maxAngle);
+        CameraAxisTransform.localEulerAngles = new Vector3(newAngleX, 0, 0);
+    }
+}
